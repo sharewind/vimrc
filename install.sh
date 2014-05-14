6ca0f5c8ae811e24ef5b77a96acc68dad95c067b
sharewind <sharewind@gmail.com>
2014-05-14 11:42:47 +0800

    fixed install

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 #wget --no-check-certificate https://raw.githubusercontent.com/sharewind/vimrc/full/install.sh -O - |sh
 
-cd ~
-git clone git@github.com:sharewind/vimrc.git
-mv vimrc .vim_runtime
-ln -sf ~/.vim_runtime/vimrcs/vimrc ~/.vimrc
+git clone -b full git@github.com:sharewind/vimrc.git ~/.vim_runtime
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim_runtime/bundle/Vundle.vim
 
+ln -sf ~/.vim_runtime/vimrcs/.vimrc ~/.vimrc
 vim +PluginInstall +qall
 
 

94f84fa1d357f0d2f7b53ad494619bd10a65bdc6
CaiJianFeng <caijianfeng@guozhenlife.cn>
2016-03-27 16:47:50 +0800

    upgrade plugins

--- install.sh
+++
@@ -1,9 +0,0 @@
-#wget --no-check-certificate https://raw.githubusercontent.com/sharewind/vimrc/full/install.sh -O - |sh
-
-git clone -b full git@github.com:sharewind/vimrc.git ~/.vim_runtime
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim_runtime/bundle/vundle
-
-ln -sf ~/.vim_runtime/vimrcs/.vimrc ~/.vimrc
-vim +PluginInstall +qall
-
-
